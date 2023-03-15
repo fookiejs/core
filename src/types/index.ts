8f33cd534a75cea7559d6b6030f61fc4704190ec
umudik <>
2023-03-16 01:17:45 +0300

    dev

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,20 @@
-import { type } from "..";
+import { type } from ".."
 import * as lodash from "lodash"
 
+export const Text: Type = (v) => lodash.isString
 
-export const Text = (v)=> lodash.isString(v)
+export const Number: Type = (v) => lodash.isNumber
+
+export const Array: Type = (v) => lodash.isArray
+
+export const Boolean: Type = (v) => lodash.isBoolean
+
+export const Buffer: Type = (v) => lodash.isBuffer
+
+export const Object: Type = (v) => lodash.isObject
+
+export const Char: Type = (v) =>
+    function (v) {
+        return lodash.isString(v) && v.length == 1
+    }
+export const Function: Type = (v) => lodash.isFunction
