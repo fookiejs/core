090b679014dd4e80d1472ba02fd49f2a930064e7
umudik <seyitumutbicer@gmail.com>
2023-04-24 02:05:19 +0300

    dev

--- packages/type/index.ts
+++ packages/type/index.ts
@@ -6,7 +6,7 @@ export const Text: Type = function (v) {
 }
 
 export const Float: Type = function (v) {
-    return lodash.isNumber(v) && Number.isInteger(v)
+    return lodash.isNumber(v)
 }
 
 export const Integer: Type = function (v) {
@@ -16,18 +16,44 @@ export const Integer: Type = function (v) {
 export const Array: Type = function (v) {
     return lodash.isArray(v)
 }
+
 export const Boolean: Type = function (v) {
     return lodash.isBoolean(v)
 }
+
 export const Buffer: Type = function (v) {
     return lodash.isBuffer(v)
 }
+
 export const Plain: Type = function (v) {
     return lodash.isObject(v)
 }
+
 export const Char: Type = function (v) {
     return lodash.isString(v) && v.length == 1
 }
+
 export const Function: Type = function (v) {
     return lodash.isFunction(v)
+}
+
+export const Timestamp: Type = function (v) {
+    return !!new Date(v)
+}
+
+export const StringArray = function (v) {
+    return lodash.isArray(v) && lodash.every(v, lodash.isString)
+}
+
+export const FloatArray = function (v) {
+    return (
+        lodash.isArray(v) &&
+        lodash.every(v, (element) => {
+            return lodash.isNumber(v)
+        })
+    )
+}
+
+export const IntegerArray = function (v) {
+    return lodash.isArray(v) && lodash.every(v, Number.isInteger)
 }
