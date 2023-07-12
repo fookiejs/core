92df7b6e0d127b3814feedb975cf46f8e0e9915a
umudik <seyitumutbicer@gmail.com>
2023-07-13 02:54:14 +0300

    dev

--- packages/mixin/src/rule/validate_attributes.ts
+++ packages/mixin/src/rule/validate_attributes.ts
@@ -2,8 +2,9 @@ import * as lodash from "lodash"
 import { LifecycleFunction } from "../../../../types"
 
 const validate_attributes: LifecycleFunction = async function (payload) {
+    const keys = lodash.keys(payload.model.schema).concat([payload.model.database.pk])
     return payload.query.attributes.every(function (k) {
-        return lodash.keys(payload.model.schema).includes(k)
+        return keys.includes(k)
     })
 }
 
