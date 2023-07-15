8d4c69b4c4e2e44ecca3962bd21daf802e7749c7
umudik <seyitumutbicer@gmail.com>
2023-07-15 13:31:31 +0300

    dev

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import * as Mixin from "./packages/mixin"
 import * as Role from "./packages/role"
 import * as Selection from "./packages/selection"
 import * as Type from "./packages/type"
-import { Fookie } from "fookie-types"
+import { Fookie } from "./types"
 
 export { Core, Database, Decorator, Method, Mixin, Role, Selection, Type, use }
 
