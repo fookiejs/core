b1fe6200bff01781a1cc35b98567f63063ff09cb
umudik <seyitumutbicer@gmail.com>
2024-07-21 19:54:02 +0300

    dev

--- src/core/mixin/src/rule/can_write.ts
+++ src/core/mixin/src/rule/can_write.ts
@@ -7,16 +7,18 @@ export default LifecycleFunction.new({
         const filtered_schema = lodash.pick(payload.schema, lodash.keys(payload.body))
 
         const writes = lodash.map(filtered_schema, function (i) {
-            return i.write
+            return i?.write
         })
 
         const filtered_writes = lodash.difference(writes, [undefined, null])
         const roles = lodash.flatten(filtered_writes)
 
         for (const role of roles) {
-            const res = await role.execute(payload)
-            if (!res) {
-                return false
+            if (role) {
+                const res = await role.execute(payload)
+                if (!res) {
+                    return false
+                }
             }
         }
 
